768d8bae2139372bc36f1edecefd8c0a10118520
max925001 <shivam969684@gmail.com>
2024-08-24 04:10:17 -0700

    env change

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 console.log(process.env.DB_PASSWORD)
 const db = mysql.createConnection({
-        host: process.env.DB_HOST || 'localhost',      // Use environment variable or fallback to 'localhost'
-    user: process.env.DB_USER || 'root',           // Use environment variable or fallback to 'root'
-    password: process.env.DB_PASSWORD || 'Pandey925@#', // Use environment variable or fallback to your password
-    database: process.env.DB_NAME || 'school_management'
+        host: process.env.DB_HOST,      // Use environment variable or fallback to 'localhost'
+    user: process.env.DB_USER,           // Use environment variable or fallback to 'root'
+    password: process.env.DB_PASSWORD, // Use environment variable or fallback to your password
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
